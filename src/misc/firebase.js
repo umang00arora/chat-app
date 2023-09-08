a9bd17ace5d5d510dab7b50f420c853ae6bdc669
umang00arora <umangarora810@gmail.com>
2023-09-08 12:39:31 +0530

    initiialized firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+const config={
+    apiKey: "AIzaSyAafWUm_gufpEFykeWkyQWXsT4erA4-9uo",
+    authDomain: "chat-web-app-916b2.firebaseapp.com",
+    projectId: "chat-web-app-916b2",
+    storageBucket: "chat-web-app-916b2.appspot.com",
+    messagingSenderId: "916011543803",
+    appId: "1:916011543803:web:ca1a3b5a1c34df8596145e"
+  };
+
+const app = firebase.initializeApp(config);
